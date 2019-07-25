e4b6fd29edba8d10caaf0a22979f2882cf6b2b51
Ravi kumar <ravikumar.pilla479@gmail.com>
2019-07-26 00:40:41 +0530

    forkifyMe final-draft

---
+++ src/js/config.js
@@ -0,0 +1,4 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+//export const key = '462b1cc8d4f2730081462fbc65136320';
+//export const key = 'f4b9678f6846d35b84fa3daed91163a2';
+export const key = '53cee344395dc44a561de75487f8d52a';
